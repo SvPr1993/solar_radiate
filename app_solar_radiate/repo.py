84c4c231a244fe2c9eae5add97bb96b7738e0cde
SvPr1993 <mrtventana@gmail.com>
2026-01-27 21:15:12 +0300

    solar_radiate

--- app_solar_radiate/repo.py
+++ app_solar_radiate/repo.py
@@ -46,3 +46,63 @@ def get_solar_activity_repo_second_api(date_str):
 
     except requests.exceptions.RequestException as e:
         return flares_info
+
+
+def get_solar_activity_data(date_str):
+    data = api_service_1(date_str)
+    if len(data) > 0:
+        print("54 строка", type(data))
+        return data, False
+    data = api_service_2(date_str)
+    if len(data) > 0:
+        print("58 строка", type(data))
+        return data, False
+    else:
+        print("61 строка", type(data))
+        return data, True
+
+#Сделать так, чтобы date_str использовалось в функциях.
+#Нужно сделать чтобы в usecase возвращались в одном формате, привести данные к одному формату, определить этот тип данных и сделать
+#Почитать про интерфесы
+def api_service_1(date_str):
+    url = settings.API_SERVICE_URL
+    flares_info = []
+
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+
+        if data:
+            for index in data:
+                index_data = {
+                    'begin_time': data.get("start_number"),
+                    'end_time': data.get("end_number"),
+                }
+                flares_info.append(index_data)
+
+                return flares_info
+
+    except requests.exceptions.RequestException as e:
+        return flares_info
+
+
+def api_service_2(date_str):
+    url = settings.API_SERVICE_URL_2
+    flares_info = {}
+
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+
+        for key, value in data.items():
+            r_data = value["R"]
+            flares_info[key] = {
+                "MinorProb": r_data.get("MinorProb"),
+                "MajorProb": r_data.get("MajorProb")
+            }
+        return flares_info
+
+    except requests.exceptions.RequestException as e:
+        return flares_info
--- app_solar_radiate/usecase.py
+++ app_solar_radiate/usecase.py
@@ -1,46 +1,43 @@
 from datetime import datetime, date
 from django.conf import settings
-from app_solar_radiate.repo import get_solar_activity_repo, get_solar_activity_repo_second_api
+from app_solar_radiate.repo import get_solar_activity_repo, get_solar_activity_repo_second_api, get_solar_activity_data
 
 
 def get_solar_activity_usecase(date_obj):
     date_str = date_obj.strftime('%Y-%m-%d')
-    flares_info = get_solar_activity_repo(date_str)
+    #flares_info = get_solar_activity_repo(date_str)
+    new_data = get_solar_activity_data(date_str)
+    return new_data
 
-    if len(flares_info) > 0:
-        return {
-            'has_data': True,
-            'date': date_str,
-            'flares_count': len(flares_info),
-            'flares': flares_info,
-            'message': f'Найдено {len(flares_info)} солнечных вспышек'
-        }
+#    if len(flares_info) > 0:
+#        return {
+#            'has_data': True,
+#            'date': date_str,
+#            'flares_count': len(flares_info),
+#            'flares': flares_info,
+#            'message': f'Найдено {len(flares_info)} солнечных вспышек'
+#        }
+#
+#    flares_info_second = get_solar_activity_repo_second_api(date_str)
+#
+#    if flares_info_second and len(flares_info_second) > 0:
+#        return {
+#            'has_data': True,
+#            'date': date_str,
+#            'flares_count_second': len(flares_info_second),
+#            'flares_second': flares_info_second,
+#           'message': f'Найдено {len(flares_info_second)} солнечных вспышек (из альтернативного источника)'
+#       }
+#       return get_solar_activity_repo_second_api(date_str)
 
-    flares_info_second = get_solar_activity_repo_second_api(date_str)
+#    else:
+#        return {
+#            'has_data': False,
+#            'date': date_str,
+#            'error': 'Сервис не работает.',
+#            'message': 'Данные о солнечной активности сейчас не доступны.'
+#        }
 
-    if flares_info_second and len(flares_info_second) > 0:
-        return {
-            'has_data': True,
-            'date': date_str,
-            'flares_count_second': len(flares_info_second),
-            'flares_second': flares_info_second,
-            'message': f'Найдено {len(flares_info_second)} солнечных вспышек (из альтернативного источника)'
-        }
-        return get_solar_activity_repo_second_api(date_str)
-
-    else:
-        return {
-            'has_data': False,
-            'date': date_str,
-            'error': 'Сервис не работает.',
-            'message': 'Данные о солнечной активности сейчас не доступны.'
-        }
-
-
-
-#Добавить второй API с солнечными вспушками в блок else
-#Если сработал блок else, то нужно добавить сюда другой репизоторой со второй API, можно в одном файле repo, если слишком много, то можно создать другой файл
-#Сделать случай если сервис вообще не работает, сделать ошибку, условно не получилось получить данный.
 
 #f'https://kauai.ccmc.gsfc.nasa.gov/DONKI/WS/get/CME?startDate={date_str}&endDate={date_str}'
 #https://services.swpc.noaa.gov/products/noaa-scales.json
