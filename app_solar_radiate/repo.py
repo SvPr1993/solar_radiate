5853defa4b303c0e5e12c19e914eebc78f30d11c
SvPr1993 <mrtventana@gmail.com>
2026-02-07 12:46:22 +0300

    solar_radiate

--- app_solar_radiate/repo.py
+++ app_solar_radiate/repo.py
@@ -3,71 +3,102 @@ import requests
 from django.conf import settings
 from datetime import datetime
 
-#Сделать так чтобы данные получались
+#+Сделать так чтобы данные получались
 #Сделать интерфейс между слоями usecase и repo с помощью нейросети
 
 class SolarActivityRepo:
-        def date_to_string(self, date_obj):
-            return date_obj.strftime('%Y-%m-%d')
+    def date_to_string(self, date_obj):
+        return date_obj.strftime('%Y-%m-%d')
 
-        def get_solar_activity_data(self, date_obj):
-            date_str = self.date_to_string(date_obj)
-            data = [] #self.api_service_1(date_obj)
-            if len(data) > 0:
-                print("54 строка", type(data))
-                return data, False
-            data = self.api_service_2(date_obj)
-            if len(data) > 0:
-                print("58 строка", type(data))
-                return data, False
-            else:
-                print("61 строка", type(data))
-                return data, True
+    def get_solar_activity_data(self, date_obj):
+        date_str = self.date_to_string(date_obj)
 
-        def api_service_2(self, date_obj):
-            date_str = self.date_to_string(date_obj)
-            url = settings.API_SERVICE_URL_2
-            flares_info = {}
-            try:
-                response = requests.get(url, timeout=5)
-                response.raise_for_status()
-                data = response.json()
+        #Запрос к первому сервису
+        data = self.api_service_1(date_obj)
+        if data and len(json.loads(data)) > 0:
+            flares_info = json.loads(data)
+            return {
+                'has_data': True,
+                'date': date_str,
+                'flares_count': len(flares_info),
+                'flares': flares_info,
+                'message': f'Найдено {len(flares_info)} солнечных вспышек'
+            }
 
+        #Запрос ко второму сервису
+        data = self.api_service_2(date_obj)
+        if data and len(json.loads(data)) > 0:
+            flares_info = json.loads(data)
+            return {
+                'has_data': True,
+                'date': date_str,
+                'flares_count_second': len(flares_info),
+                'flares_second': flares_info,
+                'message': f'Найдено {len(flares_info)} солнечных вспышек (из альтернативного источника)'
+            }
+
+        #Если оба сервиса не сработали
+        return {
+            'has_data': False,
+            'date': date_str,
+            'message': 'Нет данных о солнечных вспышках за указанную дату'
+        }
+
+    def api_service_1(self, date_obj):
+        date_str = self.date_to_string(date_obj)
+        url = settings.API_SERVICE_URL
+        flares_info = []
+
+        try:
+            response = requests.get(url, timeout=5)
+            response.raise_for_status()
+            data = response.json()
+
+            if data:
+                if isinstance(data, list):
+                    for item in data:
+                        item_data = {
+                            'begin_time': item.get("start_number"),
+                            'end_time': item.get("end_number"),
+                        }
+                        flares_info.append(item_data)
+
+                elif isinstance(data, dict):
+                    flares_info.append({
+                        'begin_time': data.get("start_number"),
+                        'end_time': data.get("end_number"),
+                    })
+
+                return json.dumps(flares_info)
+
+        except (requests.exceptions.RequestException, json.JSONDecodeError) as e:
+            return json.dumps(flares_info)
+
+    def api_service_2(self, date_obj):
+        date_str = self.date_to_string(date_obj)
+        url = settings.API_SERVICE_URL_2
+        flares_info = {}
+
+        try:
+            response = requests.get(url, timeout=5)
+            response.raise_for_status()
+            data = response.json()
+
+            if data and isinstance(data, dict):
                 for key, value in data.items():
-                    r_data = value["R"]
-                    flares_info[key] = {
-                    "MinorProb": r_data.get("MinorProb"),
-                    "MajorProb": r_data.get("MajorProb")
-                    }
+                    if isinstance(value, dict) and "R" in value:
+                        r_data = value["R"]
+                        flares_info[key] = {
+                            "MinorProb": r_data.get("MinorProb"),
+                            "MajorProb": r_data.get("MajorProb")
+                        }
 
-                json_flares_info = json.dumps(flares_info)
-                return json_flares_info
-            except requests.exceptions.RequestException as e:
-                return flares_info
+            return json.dumps(flares_info)
 
-#def api_service_1(date_obj):
-#    date_str = date_to_string(date_obj)
-#    url = settings.API_SERVICE_URL
-#    flares_info = []
-#
-#    try:
-#        response = requests.get(url, timeout=5)
-#        response.raise_for_status()
-#        data = response.json()
-#
-#        if data:
-#            for index in data:
-#                index_data = {
-#                    'begin_time': data.get("start_number"),
-#                    'end_time': data.get("end_number"),
-#                }
-#                flares_info.append(index_data)
-#                json_data = json.dumps(flares_info)
-#
-#                return json_data
-#
-#    except requests.exceptions.RequestException as e:
-#        return flares_info
+        except (requests.exceptions.RequestException, json.JSONDecodeError) as e:
+            return json.dumps(flares_info)
+
+
 
 
 
